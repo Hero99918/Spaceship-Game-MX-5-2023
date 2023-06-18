daeb626525f59ce94c426aa0774d29268b6e91d1
Hero99918 <allenlara02@gmail.com>
2023-06-18 17:35:40 -0500

    Tarea 2

---
+++ game/components/enemies/enemy2.py
@@ -0,0 +1,49 @@
+import pygame
+import random
+from game.components.enemies.enemy import Enemy
+from game.utils.constants import ENEMY_2
+from game.utils.constants import SCREEN_WIDTH, SCREEN_HEIGHT, LEFT, RIGHT
+
+class Enemy2(Enemy):
+    WIDTH = 40
+    HEIGHT = 60
+    X_POS_LIST = [100, 200, 300, 400, 500, 600, 700, 800, 900]
+    Y_POS = -3
+    SPEED_X = 6
+    SPEED_Y = 3
+    MOV_X = [LEFT, RIGHT]
+    INTERVAL = 70
+
+    def __init__(self):
+        self.image = ENEMY_2
+        self.image = pygame.transform.scale(self.image, (self.WIDTH, self.HEIGHT))
+        super().__init__(self.image)
+        self.rect.x = random.choice(self.X_POS_LIST)
+        self.rect.y = self.Y_POS
+        self.speed_x = self.SPEED_X
+        self.speed_y = self.SPEED_Y
+        self.move_x = random.choice(self.MOV_X)
+        self.index = 0
+
+    def update(self):
+        self.index += 1
+        if self.index >= self.INTERVAL:
+            self.index = 0
+            self.move_x = random.choice(self.MOV_X)
+        
+        if self.move_x == LEFT:
+            self.rect.x -= self.speed_x
+            if self.rect.x <= 0:
+                self.move_x = RIGHT
+        elif self.move_x == RIGHT:
+            self.rect.x += self.speed_x
+            if self.rect.x + self.WIDTH > SCREEN_WIDTH:
+                self.rect.x = SCREEN_WIDTH - self.WIDTH
+                self.move_x = LEFT
+
+        self.rect.y += self.speed_y
+        if self.rect.y > SCREEN_HEIGHT:
+            self.is_alive = False
+
+    def draw(self, screen):
+        screen.blit(self.image, (self.rect.x, self.rect.y))
--- game/components/enemies/enemy_handler.py
+++ game/components/enemies/enemy_handler.py
@@ -1,11 +1,12 @@
 from game.components.enemies.ship import ship
+from game.components.enemies.enemy2 import Enemy2
 
 class EnemyHandler:
     def __init__(self):
         self.enemies = []
 
     def update(self):
-        self.add_enemy()
+        self.add_enemy() and self.add_enemy2
         for enemy in self.enemies:
             enemy.update()
             if not enemy.is_alive:
@@ -18,6 +19,10 @@ class EnemyHandler:
     def add_enemy(self):
         if len(self.enemies) < 4:
             self.enemies.append(ship())
+            self.enemies.append(Enemy2())
 
     def remove_enemy(self, enemy):
         self.enemies.remove(enemy)
+
+    def check_collision(rect1, rect2):
+        return rect1.colliderect(rect2)
